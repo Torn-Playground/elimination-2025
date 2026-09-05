4435e798916f8047c1d3e41d82d84230c90d4d09
Kobe De Peuter <development@kobedepeuter.be>
2026-09-05 23:39:37 +0200

    fix: resolve verify link

--- src/config.ts
+++ src/config.ts
@@ -26,4 +26,5 @@ export const VERIFIED_ROLE_NAME = "Verified";
 
 export const NOT_VERIFIED_CHANNEL_NAME = "not-verified";
 
-export const TORN_VERIFY_URL = "https://discord.com/channels/681922682949992581/909160345736728647/1533191612140753058";
+export const TORN_VERIFY_URL =
+    "https://discord.com/api/oauth2/authorize?client_id=439014098987122698&redirect_uri=https%3A%2F%2Fwww.torn.com%2Fdiscord.php&response_type=code&scope=identify";
